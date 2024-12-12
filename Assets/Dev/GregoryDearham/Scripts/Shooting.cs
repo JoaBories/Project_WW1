2baab441dd939bfe2b20e729c152a0d100e10ca6
Do0vm <125159238+Do0vm@users.noreply.github.com>
2024-12-12 21:51:08 +0200

    Shooting Functionality

--- Assets/Dev/GregoryDearham/Scripts/Shooting.cs
+++ Assets/Dev/GregoryDearham/Scripts/Shooting.cs
@@ -1,121 +1,69 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    public GameObject normalProjectilePrefab; 
-    public GameObject chargedProjectilePrefab; 
-    public Transform firePoint; 
-    public float projectileSpeed = 10f; 
-    public float chargeTime = 1.5f; 
-
-    private bool isCharging = false; 
-    private float chargeTimer = 0f;
-    private bool facingleft = false;
-
-
-
-
-
-    private void Awake()
-    {
-        
-    }
-
-
+    public GameObject normalProjectilePrefab;
+    public Transform firePoint;
+    public float projectileSpeed = 0.97f;
+    public Animator animator;
+    [SerializeField] public float projectileDistance = 0.5f;
+    
+    private bool facingleft = false; // Detects the facing direction
+    private bool isShooting = false; // Tracks whether the character is shooting
 
     void Update()
-
     {
-        if (NewMovement.instance.CheckGround())
-        {
-            HandleShootingInput();
-
-        }
-
-
-        
-    
-        
-
-        
-
+        HandleShootingInput();
     }
 
     void HandleShootingInput()
     {
-
-
-
-
-
-  if (Input.GetKeyDown(KeyCode.Mouse0))
+       
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !isShooting) 
         {
-            isCharging = true;
-            chargeTimer = 0f;
-        }
-
-
-        if (Input.GetKey(KeyCode.Mouse0) && isCharging)
-        {
-            chargeTimer += Time.deltaTime;
-        }
-
-
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            if (chargeTimer >= chargeTime)
-            {
-                /*ShootChargedProjectile()*/
-
-            }
-            else
-            {
-                ShootNormalProjectile();
-
-                isCharging = false;
-            }
-        }
-
-        void ShootNormalProjectile()
-        {
-            GameObject projectile = Instantiate(normalProjectilePrefab, firePoint.position, Quaternion.identity);
-            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-
-
-            if (GetComponent<SpriteRenderer>().flipX)
-
-            {
-
-                    rb.velocity = -firePoint.right * projectileSpeed;
-
-
-
-
-            }
-
-            else
-            {
-                rb.velocity = firePoint.right * projectileSpeed;
-
-            }
             
+            isShooting = true;
+            NewMovement.instance.lockMovements(); 
+            Actions.Instance.LockGameplay();     
 
-            Destroy(projectile, 0.08f);
+            
+            animator.SetTrigger("Attack");
+
+            
+            ShootNormalProjectile();
+
+           
+            StartCoroutine(ResetShooting());
+        }
+    }
+
+    void ShootNormalProjectile()
+    {
+        GameObject projectile = Instantiate(normalProjectilePrefab, firePoint.position, Quaternion.identity);
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+
+        
+        if (GetComponent<SpriteRenderer>().flipX)
+        {
+            rb.velocity = -firePoint.right * projectileSpeed;
+        }
+        else
+        {
+            rb.velocity = firePoint.right * projectileSpeed;
         }
 
-      
+        
+        Destroy(projectile, projectileDistance);
+    }
 
+    System.Collections.IEnumerator ResetShooting()
+    {
+        
+        yield return new WaitForSeconds(0.5f); 
 
-
-        //void ShootChargedProjectile()
-        //{
-        //    GameObject chargedProjectile = Instantiate(chargedProjectilePrefab, firePoint.position, Quaternion.identity);
-        //    Rigidbody2D rb = chargedProjectile.GetComponent<Rigidbody2D>();
-        //    rb.velocity = firePoint.right * projectileSpeed * 1.5f;
-        //    chargedProjectile.GetComponent<SpriteRenderer>().color = Color.yellow;
-
-        //    Destroy(chargedProjectile, 0.08f);
-        //}
+       
+        NewMovement.instance.delockMovements(); 
+        Actions.Instance.DelockGameplay();     
+        isShooting = false;
     }
 }
