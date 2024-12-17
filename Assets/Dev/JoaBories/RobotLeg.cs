199b6cda8885a440e983501cc5b767a5c4e2a298
Joa Bories <joa.bories@gmail.com>
2024-12-17 17:55:43 +0100

    [add] begin of the robot

--- Assets/Dev/JoaBories/RobotLeg.cs
+++ Assets/Dev/JoaBories/RobotLeg.cs
@@ -2,17 +2,93 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SmashPhases
+{
+    goingUp,
+    stayingUp,
+    goingRight,
+    goingDown,
+    stayingDown
+}
+
 public class RobotLeg : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject floorWaypoint;
+    [SerializeField] private GameObject skyWaypoint;
+
+    [SerializeField] private float upSpeed, downSpeed , sideSpeed;
+
+    [SerializeField] private List<GameObject> smashWaypoints;
+    private int currenSmashWaypointIndex;
+
+    private float nextStayingEnd;
+    [SerializeField] private float stayingTime;
+
+    [SerializeField] private SmashPhases phase;
+
+    private void Awake()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        phase = SmashPhases.stayingDown;
+        nextStayingEnd = Time.time + stayingTime;
     }
+
+    private void Update()
+    {
+        switch (phase)
+        {
+            case SmashPhases.goingUp:
+                transform.position += new Vector3(0, upSpeed * Time.deltaTime, 0);
+                if(transform.position.y >= floorWaypoint.transform.position.y)
+                {
+                    phase = SmashPhases.goingRight;
+                    transform.position = new Vector3(transform.position.x, floorWaypoint.transform.position.y, transform.position.z);
+                }
+                break;
+
+            case SmashPhases.goingDown:
+                transform.position += new Vector3(0, -upSpeed * Time.deltaTime, 0);
+                if (transform.position.y >= floorWaypoint.transform.position.y)
+                {
+                    phase = SmashPhases.stayingDown;
+                    transform.position = new Vector3(transform.position.x, floorWaypoint.transform.position.y, transform.position.z);
+                }
+                break;
+
+            case SmashPhases.goingRight:
+                transform.position += new Vector3(sideSpeed * Time.deltaTime, 0, 0);
+                if (transform.position.x >= smashWaypoints[currenSmashWaypointIndex].transform.position.x)
+                {
+                    phase = SmashPhases.stayingUp;
+                    transform.position = new Vector3(smashWaypoints[currenSmashWaypointIndex].transform.position.x, transform.position.y, transform.position.z);
+                    if (currenSmashWaypointIndex < smashWaypoints.Count)
+                    {
+                        currenSmashWaypointIndex++;
+                    }
+                    nextStayingEnd = Time.time + stayingTime;
+                }
+                break;
+
+            case SmashPhases.stayingDown:
+                if(Time.time > nextStayingEnd)
+                {
+                    phase = SmashPhases.goingUp;
+                }
+                break;
+
+            case SmashPhases.stayingUp:
+                if(Time.time > nextStayingEnd)
+                {
+                    phase = SmashPhases.goingDown;
+                }
+                break;
+        }
+
+
+    }
+
 }
