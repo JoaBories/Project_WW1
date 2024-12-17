94cf6d83ddfc4023625f6b36965d9e14f7b1ecc2
Gregory Dearham <125159238+Do0vm@users.noreply.github.com>
2024-12-17 17:38:41 +0100

    [Add] Ene,nemy ver 1

--- Assets/Dev/GregoryDearham/Scripts/EnemyStun.cs
+++ Assets/Dev/GregoryDearham/Scripts/EnemyStun.cs
@@ -1,24 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class EnemyStun : MonoBehaviour
 {
     public static EnemyStun instance;
     public bool Stun = false;
     private Animator _animator;
-    [SerializeField]  private Canvas childCanvas;
-    
+
+    private Canvas childCanvas;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
         instance = this;
+
+        // Find the Canvas only within this GameObject's immediate children
+        childCanvas = transform.Find("Canvas")?.GetComponent<Canvas>();
+
+        if (childCanvas == null)
+        {
+            Debug.LogError("No child Canvas found under this Enemy prefab!");
+        }
     }
-
-
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -26,18 +28,20 @@ public class EnemyStun : MonoBehaviour
         {
             Debug.Log("Dead");
             _animator.Play("EnemyStun");
-            
+
             Stun = true;
 
-            Destroy(childCanvas);
-            
-
+            // Destroy the specific child Canvas GameObject
+            if (childCanvas != null)
+            {
+                Destroy(childCanvas.gameObject);
+                Debug.Log("Child Canvas Destroyed");
+            }
         }
-
-
     }
 
-    
-
-
+    public void killSoldier()
+    {
+        Destroy(gameObject);
+    }
 }
